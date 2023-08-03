b252a25aad3a6c8440033ee607b40fa39b59cffb
gradan-hash <mutukunyaa@gmail.com>
2023-08-03 12:48:40 +0300

    easyyyy mff

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
 
@@ -7,6 +8,17 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/submit", methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['radio']
+        comments = request.form['comments']
+        print(customer, dealer, rating, comments)
+        return render_template('success.html')
+
+
 if __name__ == "__main__":
-  
+
     app.run(debug=True)
